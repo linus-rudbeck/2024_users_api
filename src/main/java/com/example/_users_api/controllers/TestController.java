7f4191ce666c8c68faa501272f05260c0a0ad42a
linus-rudbeck <linus.rudbeck@distansakademin.se>
2024-01-09 15:41:18 +0100

    Added TestController

--- src/main/java/com/example/_users_api/controllers/TestController.java
+++ src/main/java/com/example/_users_api/controllers/TestController.java
@@ -1,2 +1,13 @@
-package com.example._users_api.controllers;public class TestController {
+package com.example._users_api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String getTest(){
+        return "THis is a test controller that we add to a separate branch";
+    }
 }
