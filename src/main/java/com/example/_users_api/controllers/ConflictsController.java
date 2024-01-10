316a39fcf2977eff770aa521019fd58a952e34be
linus-rudbeck <linus.rudbeck@distansakademin.se>
2024-01-10 09:11:14 +0100

    Added delete conflicts method

--- src/main/java/com/example/_users_api/controllers/ConflictsController.java
+++ src/main/java/com/example/_users_api/controllers/ConflictsController.java
@@ -1,5 +1,6 @@
 package com.example._users_api.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class ConflictsController {
     @GetMapping("/conflicts")
     public String getConflicts(){
         return "Getting conflicts";
+    }
+
+    @DeleteMapping("/conflicts")
+    public String deleteConflicts(){
+        return "Deleting conflicts";
     }
 }
