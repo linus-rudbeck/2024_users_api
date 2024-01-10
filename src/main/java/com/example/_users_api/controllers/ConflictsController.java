179ea96840c076b617cf1588c4be157e4251003c
linus-rudbeck <linus.rudbeck@distansakademin.se>
2024-01-10 08:57:29 +0100

    Created Conflicts Controller

---
+++ src/main/java/com/example/_users_api/controllers/ConflictsController.java
@@ -0,0 +1,13 @@
+package com.example._users_api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConflictsController {
+
+    @GetMapping("/conflicts")
+    public String getConflicts(){
+        return "Getting conflicts";
+    }
+}
