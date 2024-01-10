1291a5ccd426571e96625e2a744fd1fc80c15456
linus-rudbeck <linus.rudbeck@distansakademin.se>
2024-01-10 09:46:34 +0100

    Update ConflictsController.java

--- src/main/java/com/example/_users_api/controllers/ConflictsController.java
+++ src/main/java/com/example/_users_api/controllers/ConflictsController.java
@@ -1,9 +1,6 @@
 package com.example._users_api.controllers;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ConflictsController {
@@ -21,5 +18,10 @@ public class ConflictsController {
     @PostMapping("/conflicts")
     public String postConflicts(){
         return "Posting conflicts";
+    }
+
+    @PutMapping("/conflicts")
+    public String putConflicts(){
+        return "Putting conflicts";
     }
 }
