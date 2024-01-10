5a6458a650cc2d77dbc2afd560b5499a5279d4dc
linus-rudbeck <linus.rudbeck@distansakademin.se>
2024-01-10 09:04:53 +0100

    Created post conflicts method

--- src/main/java/com/example/_users_api/controllers/ConflictsController.java
+++ src/main/java/com/example/_users_api/controllers/ConflictsController.java
@@ -1,6 +1,7 @@
 package com.example._users_api.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class ConflictsController {
     @GetMapping("/conflicts")
     public String getConflicts(){
         return "Getting conflicts";
+    }
+
+    @PostMapping("/conflicts")
+    public String postConflicts(){
+        return "Posting conflicts";
     }
 }
